6500cbc3ae5d63b4355fcbe920f57263d21adcc4
Ashansh Lokhande <ashansh.lokhande@viacomcontractor.com>
2020-05-23 04:01:45 +0530

    changed images relative path

--- Resume/src/app/project-cards/project-cards.component.ts
+++ Resume/src/app/project-cards/project-cards.component.ts
@@ -9,7 +9,7 @@ export class ProjectCardsComponent {
   public projectDataArray = [
     {
       title: 'KNN on NYPD Crime Data',
-      imageUrl : '../assets/images/nypd.jpg',
+      imageUrl : 'assets/images/nypd.jpg',
       introduction: 'This dataset includes all valid felony, misdemeanor, and violation crimes reported to the New York City Police Department (NYPD) from 2006 to October 25 2019.',
       description: ' The dataset is updated annually and is updated and maintained by Police Department (NYPD). The Goal of the project is to predict the level of offense (felony, misdemeanor, violation) depending on different factors associated with the registered complaint and analyze the factors in the dataset leading to the increase or decrease of crime rates in the dataset. The target variable (Level of offense) is categorical in nature and thus Generalized Logistic regression and K-Nearest Neighbor method can be implemented to predict the class of the target variable.',
       link: 'https://github.com/Swapnillokhande/KNN-on-NYPD-Crime-data',
